1ff91f22a1cdd5ca4fb37c8fc9bd3108bb70974c
FladeX <FladeX@yandex.ru>
2013-12-24 00:22:10 +0700

    add second slider

--- vstk-selhoz/js/main.js
+++ vstk-selhoz/js/main.js
@@ -31,4 +31,33 @@ $(document).ready(function () {
 	setInterval(function() {
 		slider();
 	}, slider_circle);
+
+	// bottom slider
+	var partners_width = 0,
+		partners_step = 500, // pixels
+		partners_left = 0;
+	$('.b-partners__content img').each(function(){
+		partners_width += $(this).outerWidth(true);
+	});
+	partners_width += 30;
+	$('.b-partners__content').css('width', partners_width);
+	$('.b-partners__arrow').on('click', function(){
+		partners_left = parseInt($('.b-partners__content').css('left'));
+		if ($(this).hasClass('b-partners__arrow_dir_left')) {
+			if (Math.abs(partners_left) < partners_step) {
+				partners_left = 0;
+			} else {
+				partners_left += partners_step;
+			}
+		} else {
+			if (partners_width + partners_left < 960) {
+				partners_left = 0;
+			} else {
+				partners_left -= partners_step;
+			}
+		}
+		$('.b-partners__content').animate({
+			'left': partners_left + 'px'
+		});
+	});
 });
